9aa6fa18ad310ecdd71fddab31c16a254ec1b205
Fynn Borja <pewdiestuff@gmail.com>
2024-06-30 20:37:43 +0800

    [CHANGE] Set Side Pages Overflow from Scroll to Auto

--- src/pages/GleamProject.jsx
+++ src/pages/GleamProject.jsx
@@ -5,7 +5,7 @@ import ArrowSvg from "../components/ui/Home/svg/ArrowSvg";
 const GleamProject = () => {
     return (
         <div className="bg-teal-800 w-screen h-screen flex justify-center shadow-xl">
-            <div className="overflow-y-scroll sm:w-5/6 md:w-4/5 lg:w-3/5 xl:w-2/5 2xl:w-2/6 my-4 border shadow-lg rounded-xl bg-white p-6 flex flex-col">
+            <div className="overflow-y-auto sm:w-5/6 md:w-4/5 lg:w-3/5 xl:w-2/5 2xl:w-2/6 my-4 border shadow-lg rounded-xl bg-white p-6 flex flex-col">
                 <div className="flex justify-between">
                     <Link to={"/"} className="border flex gap-2 items-center py-1 px-2 rounded-2xl shadow-sm transition-all duration-100 hover:scale-105">
                         <BackSVG/>
--- src/pages/LeetcodeProject.jsx
+++ src/pages/LeetcodeProject.jsx
@@ -4,7 +4,7 @@ import BackSVG from "../components/ui/Projects/BackSVG";
 const LeetcodeProject = () => {
     return (
         <div className="bg-gray-400 w-screen h-screen flex justify-center shadow-xl">
-            <div className="overflow-y-scroll sm:w-5/6 md:w-4/5 lg:w-3/5 xl:w-2/5 2xl:w-2/6 my-4 border shadow-lg rounded-xl bg-white p-6 flex flex-col">
+            <div className="overflow-y-auto sm:w-5/6 md:w-4/5 lg:w-3/5 xl:w-2/5 2xl:w-2/6 my-4 border shadow-lg rounded-xl bg-white p-6 flex flex-col">
                 <div className="flex justify-between">
                     <Link to={"/"} className="border flex gap-2 items-center py-1 px-2 rounded-2xl shadow-sm transition-all duration-100 hover:scale-105">
                         <BackSVG/>
