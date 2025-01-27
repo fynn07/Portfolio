80f14040c1fa41b5fd782f601a8a290833cd16a2
Fynn <pewdiestuff@gmail.com>
2025-01-27 21:10:28 +0800

    Changed Profile Picture

--- src/components/ui/Navbar/ProfileContent.jsx
+++ src/components/ui/Navbar/ProfileContent.jsx
@@ -1,7 +1,7 @@
 const ProfileContent = () => {
     return (
         <div className="flex flex-col justify-center items-center">
-            <img className="w-20 rounded-full mb-2 2xl:w-28" src="./assets/self_portrait.jpg" alt="self portrait" />
+            <img className="w-20 rounded-full mb-2 2xl:w-28" src="./assets/me.jpeg" alt="self portrait" />
             <p className="text-primary text-md font-poppins font-semibold hidden lg:inline 2xl:text-xl">Fynn Borja</p>
             <p className="text-secondary text-sm font-poppins font-medium hidden lg:inline 2xl:text-lg">Software Engineer</p>
         </div>
